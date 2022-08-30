5c89b52836f4639f1cc4208925f3d37e1bf0686a
mireyalyons <90634951+mireyalyons@users.noreply.github.com>
2022-08-30 14:32:27 -0700

    Add files via upload

---
+++ 3 - Using Python to Access Web Data/Extracting Data With Regular Expressions/ch11assignment.py
@@ -0,0 +1,25 @@
+#Code for practice data
+import re
+
+doc = open('py4ech11sampledata.txt')
+
+sum = 0
+
+for line in doc:
+    numbers = re.findall('[0-9]+', line)
+    for number in numbers:
+        sum = sum + int(number)
+
+print(sum)
+
+#Code for actual data
+doc2 = open('ch11actualdata.txt')
+
+sum2 = 0
+
+for line in doc2:
+    numbers2 = re.findall('[0-9]+', line)
+    for number2 in numbers2:
+        sum3 = sum2 + int(number2)
+
+print(sum3)
