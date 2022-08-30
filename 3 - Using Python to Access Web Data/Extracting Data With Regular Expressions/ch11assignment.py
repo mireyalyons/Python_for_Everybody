cbf78ed049a9ff4cb351962902474538fc958823
mireyalyons <90634951+mireyalyons@users.noreply.github.com>
2022-08-30 15:10:22 -0700

    Update ch11assignment.py

--- 3 - Using Python to Access Web Data/Extracting Data With Regular Expressions/ch11assignment.py
+++ 3 - Using Python to Access Web Data/Extracting Data With Regular Expressions/ch11assignment.py
@@ -1,4 +1,23 @@
-#Code for practice data
+# Run seperately
+
+#Code for Chaper 11 Assignment Data
+
+import re
+
+doc = open('regex_sum_1419494.txt')
+
+sum = 0
+
+for line in doc:
+    numbers = re.findall('[0-9]+', line)
+    for number in numbers:
+        sum = sum + int(number)
+
+print(sum)
+
+
+#Code for Practice Data
+
 import re
 
 doc = open('regex_sum_42.txt')
@@ -11,15 +30,3 @@ for line in doc:
         sum = sum1 + int(number)
 
 print(sum)
-
-#Code for assignment data
-doc2 = open('regex_sum_1419494.txt')
-
-sum2 = 0
-
-for line in doc2:
-    numbers2 = re.findall('[0-9]+', line)
-    for number2 in numbers2:
-        sum3 = sum2 + int(number2)
-
-print(sum3)
