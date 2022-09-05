ffa39643cf68c21c0cabddae92e27d1268bc62e6
Mireya Lyons <90634951+mireyalyons@users.noreply.github.com>
2022-09-05 13:56:37 -0700

    Update Scraping W BeautifulSoup.py

--- 3 - Using Python to Access Web Data/Scraping HTML Data with BeautifulSoup/Scraping W BeautifulSoup.py
+++ 3 - Using Python to Access Web Data/Scraping HTML Data with BeautifulSoup/Scraping W BeautifulSoup.py
@@ -1,48 +1,22 @@
-#first version
-#import urllib.request, urllib.parse, urllib.error
-#import xml.etree.ElementTree as ET
-#url = ‘http://py4e-data.dr-chuck.net/comments_269101.xml’
-#uh = urllib.request.urlopen(url)
-#data = uh.read()
-#tree = ET.fromstring(data)
-#counts = tree.findall(‘.//count’)
-#counts_list = [int(count.text) for count in counts]
-#print(sum(counts_list))
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
 
+url = input('Enter - ')
+html = urlopen(url,).read()
+soup = BeautifulSoup(html, "html.parser")
 
-#version i like better
-#import urllib.request, urllib.parse, urllib.error
-#import xml.etree.ElementTree as ET
+# Retrieve all of the anchor tags
+tags = soup('span')
+numlist = list()
+for tag in tags:
+    # Look at the parts of a tag
+    y = str(tag)
+    num = re.findall('[0-9]+',y)
+    numlist = numlist + num
 
-#url = 'http://py4e-data.dr-chuck.net/comments_42.xml'
+sum = 0
+for i in numlist:
+    sum = sum + int(i)
 
-#uh = urllib.request.urlopen(url)
-#data = uh.read()
-
-#tree = ET.fromstring(data)
-
-#lst = tree.findall('comments/comment/count')
-
-#counts = tree.findall('.//count')
-
-#for each in counts:
-#    print(each.text)
-
-from urllib import request
-import xml.etree.ElementTree as ET
-
-url = 'http://py4e-data.dr-chuck.net/comments_1419496.xml'
-print ("Retrieving", url)
-html = request.urlopen(url)
-data = html.read()
-print("Retrieved",len(data),"characters")
-
-tree = ET.fromstring(data)
-results = tree.findall('comments/comment')
-count=len(results)
-sum=0
-
-for result in results:
-    sum += float(result.find('count').text)
-print(count)
 print(sum)
