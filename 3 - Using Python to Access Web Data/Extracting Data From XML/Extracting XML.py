f41e96401eae24a09a22079e0de99553922b6206
Mireya Lyons <90634951+mireyalyons@users.noreply.github.com>
2022-09-05 14:01:16 -0700

    Update Extracting XML.py

--- 3 - Using Python to Access Web Data/Extracting Data From XML/Extracting XML.py
+++ 3 - Using Python to Access Web Data/Extracting Data From XML/Extracting XML.py
@@ -1,35 +1,3 @@
-#first version
-#import urllib.request, urllib.parse, urllib.error
-#import xml.etree.ElementTree as ET
-#url = ‘http://py4e-data.dr-chuck.net/comments_269101.xml’
-#uh = urllib.request.urlopen(url)
-#data = uh.read()
-#tree = ET.fromstring(data)
-#counts = tree.findall(‘.//count’)
-#counts_list = [int(count.text) for count in counts]
-#print(sum(counts_list))
-
-
-
-
-#version i like better
-#import urllib.request, urllib.parse, urllib.error
-#import xml.etree.ElementTree as ET
-
-#url = 'http://py4e-data.dr-chuck.net/comments_42.xml'
-
-#uh = urllib.request.urlopen(url)
-#data = uh.read()
-
-#tree = ET.fromstring(data)
-
-#lst = tree.findall('comments/comment/count')
-
-#counts = tree.findall('.//count')
-
-#for each in counts:
-#    print(each.text)
-
 from urllib import request
 import xml.etree.ElementTree as ET
 
